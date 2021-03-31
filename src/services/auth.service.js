8d9cf0f98e2f4152d6e9eae3053c066926e2d968
keep1tstupid <alexandra.globa@gmail.com>
2021-03-31 17:12:13 +0300

    auth service updated

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/auth/";
+const API_URL = "https://bboard-app.herokuapp.com/api/auth/";
 
 const login = (username, password) => {
   console.log(username, password, );
